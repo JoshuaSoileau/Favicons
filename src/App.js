f039e41c6db1b3086e8534a7f0cc5416dcf13f39
Joshua Soileau <joshua.soileau@gmail.com>
2021-01-08 17:44:43 -0500

    Adds a footer

--- src/App.js
+++ src/App.js
@@ -13,11 +13,38 @@ function App() {
     <AppProvider>
       <GlobalStyles />
 
-      <main tw="bg-gradient-to-r from-blue-50 min-h-screen flex flex-col items-center">
-        <div tw="min-h-screen grid lg:grid-cols-2 gap-32 items-center  max-w-7xl  mx-auto  px-8">
-          <Left />
-          <Right />
+      <main tw="bg-gradient-to-r from-blue-50  min-h-screen">
+        <div
+          style={{ minHeight: "calc(100vh - 57px)" }}
+          tw="flex flex-col justify-center"
+        >
+          <div tw="grid lg:grid-cols-2 gap-32 items-center  max-w-7xl  mx-auto  px-8">
+            <Left />
+            <Right />
+          </div>
         </div>
+        <footer tw="flex justify-center items-center py-4  border-t border-solid border-gray-200 w-full">
+          Made with
+          <span role="img" aria-label="anger" tw="mx-2 mr-3">
+            💢
+          </span>
+          {` by `}
+          <a
+            tw="font-bold text-purple-500  hover:(text-purple-800 underline)  ml-1"
+            href="https://twitter.com/joshua_soileau"
+          >
+            Josh
+          </a>
+          {`. View the`}
+          &nbsp;
+          <a
+            tw="font-bold text-purple-500  hover:(text-purple-800 underline)"
+            href="https://github.com/JoshuaSoileau/Favicons"
+          >
+            source
+          </a>
+          .
+        </footer>
       </main>
     </AppProvider>
   );
